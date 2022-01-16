b26d46ba1e10f65406e599d3e37fea51b56f2388
JovanCorilic <kssbc39@gmail.com>
2022-01-16 20:55:22 +0100

    inicijalizacija dva

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+# 1 - kreacija , 2 - promena, 3 - logicko brisanje, 4 - pravo brisanje
+ATTRIBUTES = ["id", "ime_i_prezime", "datum_i_vreme","oznaka_spasioca", "trajanje_spasavanja", "status","svrha"]
+FMT = "i60s20s5siii"
+CODING = "ascii"
+
+F = 5
+B = 9
+k = 1
--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 if __name__ == '__main__':
-    print("Hello there!")
+    binary_file = ""
+    binary_file_izlazna = ""
+    binary_file_greska = ""
+    while True:
+        print("Sta zelite od opcija (unesite broj da odaberete, 0 za iskljucivanje programa):\n"
+              "1.Formiranje prazne datoteke\n"
+              "2.Izbor aktivne datoteke zadavanjem njenog naziva\n"
+              "3.Prikaz naziva aktivne datoteke\n"
+              "4.Pocni operaciju\n"
+              "5.Ispis aktivne, izlazne i datoteke gresaka\n"
+              "6.Ucitati test primer")
+        unos = int(input("Unesite ovde opciju:"))
+        if (unos == 0):
+            break
+        if (unos == 1):
+            naziv = "data/" + str(input("Unesite naziv datoteke:")) + ".dat"
+
+
+        if (unos == 2):
+            naziv = "data/" + str(input("Unesite naziv aktivne datoteke:")) + ".dat"
+
+        if (unos == 3):
+            if binary_file != "":
+                print("Naziv fajla: " + binary_file.filename.split("/")[1])
+            else:
+                print("Niste odabrali aktivni fajl!")
